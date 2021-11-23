dad4e17bcf68327e4d7e9f9ce397e560deb17086
Alejo Vera <vera.alejo0903@gmail.com>
2021-11-23 10:43:37 -0300

    [FIX: env and package.json]

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 
+require("dotenv").config();
+
 const app = express()
 
-app.get('/', (req, res) => {
-
-})
+app.use(express.static('public'))
 
 app.get('/api/posts', cors(), (req, res) => {
     const data = 
@@ -1700,6 +1700,8 @@ app.get('/api/posts', cors(), (req, res) => {
   });
   
 
-const port = 5000;
+const port = process.env.PORT || 3001;
 
-app.listen(process.env.PORT || 5000);
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
